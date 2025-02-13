a40f33956735303dd972f07fb9dc8128cf35813e
eunho <76768140sa@gmail.com>
2025-02-13 23:33:16 +0900

    practice function scope

--- 08-Behind-the-Scenes/Scoping in Practice/script.js
+++ 08-Behind-the-Scenes/Scoping in Practice/script.js
@@ -2,34 +2,32 @@
 
 function calcAge(birthYear) {
     const age = 2037 - birthYear;
-    console.log(firstName)
-
+    
     function printAge() {
-        const output = `${firstName} You are ${age}, born in ${birthYear}`;
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
         console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+        }
+
+        // console.log(str); // 접근 불가
+        console.log(millenial); // var로 선언된 변수는 함수 내부 어디서든 접근 가능
+        // console.log(add(2, 3)); // add 함수는 if 블록 내부에 선언되었지만, 함수 호이스팅으로 인해 접근 가능
     }
+
     printAge();
+    return age;
+}
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-        var millenial = true;
-        // Creating NEW variable with same name as outer scope's variable
-        const firstName = 'Steven';
-
-        // Reasssigning outer scope's variable
-        output = 'NEW OUTPUT!';
-
-        const str = `Oh, and you're a millenial, ${firstName}`;
-        console.log(str);
-
-        function add(a, b) {
-            return a + b;
-        } k
-
-        return age;
-    }
-
-    const firstName = 'Jonas';
-    calcAge(1991);
+const firstName = "John";
+calcAge(1991);
 
 // parent scope -> child scope 접근 불가능
 // console.log(age);
