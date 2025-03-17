8f79d4cc6b711dd13bbc25e646ab671b218f1c6a
eunho <76768140sa@gmail.com>
2025-03-17 23:52:02 +0900

    For Each

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -65,16 +65,10 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 console.log(arr.slice(2));
@@ -101,3 +95,52 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join('-'));
+*/
+
+// const arr = [23, 11, 64];
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+
+// console.log('Jonas'.at(0));
+
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else { 
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('------- FOREACH -------')
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else { 
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+})
+// 0: function (200)
+// 1: function (450)
+// ...
+*/
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key} : ${value}`);
+});
+
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function(value, key, set) {
+  console.log(`${value} : ${value}`);
+});
